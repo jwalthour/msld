9873306d3e2279643ef58829876092c1095a617e
John Walthour <john.walthour@gmail.com>
2021-12-15 21:04:19 -0500

    Added actual button assignments.  Confirmed working with real HW.  Buttons are active low.

--- button_test.py
+++ button_test.py
@@ -11,9 +11,10 @@ def falling_edge(channel):
 
 
 
-bcm_pin_for_btn = [27, 22, 23, 24]
+#bcm_pin_for_btn = [27, 22, 23, 24]
 #bcm_pin_for_btn = [27]
+bcm_pin_for_btn = [14,15,25,19]
 for pin in bcm_pin_for_btn:
     GPIO.setup(pin, GPIO.IN, pull_up_down=GPIO.PUD_UP)
-    # GPIO.add_event_detect(pin, GPIO.RISING, callback=rising_edge)
+    #GPIO.add_event_detect(pin, GPIO.RISING, callback=rising_edge)
     GPIO.add_event_detect(pin, GPIO.FALLING, callback=falling_edge)
