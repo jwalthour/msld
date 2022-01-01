eb2e7712a8a09e63551ccff065cdbbb41a974529
John Walthour <john.walthour@gmail.com>
2022-01-01 12:55:45 -0500

    Dimmed from 75% to 50%

--- system_files/start.sh
+++ system_files/start.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 cd /mnt/source/msld
-sudo python main.py --led-gpio-mapping=adafruit-hat-pwm --led-rows 32 --led-cols 64 --led-slowdown-gpio=2 --led-row-addr-type 0 --led-brightness=75
+sudo python main.py --led-gpio-mapping=adafruit-hat-pwm --led-rows 32 --led-cols 64 --led-slowdown-gpio=2 --led-row-addr-type 0 --led-brightness=50
